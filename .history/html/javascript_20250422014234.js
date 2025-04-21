c07a9b9c955d7acfe138eaa6ec717bf32cfd25c3
BANKOZAKA1 <banklaysoulivong123@gmail.com>
2025-04-22 01:46:44 +0700

    testcode01

--- .history/html/javascript_20250422014234.js
+++ .history/html/javascript_20250422014234.js
@@ -1,4 +1,3 @@
 console.log("Hello World");
 console.log("Hello from JavaScript!");
 console.log("This is a test of the JavaScript console.");
-console.log("This is a test of the JavaScript console.");
